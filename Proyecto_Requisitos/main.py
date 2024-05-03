129e4a416c706a44d1cf2db32f74440609ed3625
JuanSO121 <sanchezjuanjose0508@gmail.com>
2024-05-03 10:33:59 -0500

    first commit

---
+++ Proyecto_Requisitos/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from utils import *
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Extract form data
+    deliverable = request.form.get('deliverable')
+    subject = request.form.get('subject')
+    topic = request.form.get('topic')
+    textModel = request.form.get('textModel') 
+    audioVoice = request.form.get('audioVoice')
+    temperature = float(request.form.get('temperature')) 
+
+    
+    audioPrompt = format_audio_dict(deliverable, subject, topic, textModel, temperature, audioVoice)
+    audioResponse = text_audio_models(audioPrompt)
+  
+    textPrompt = format_text_dict(deliverable, subject, topic, textModel, temperature, format="HTML")
+    htmlContent = text_text_models(textPrompt)
+    
+    return render_template('result.html', topic=topic, htmlInfo=htmlContent, audioPrompt = audioPrompt["input"])
+
+
+app.run(host='0.0.0.0', port=81)
---
+++ Proyecto_Requisitos/utils.py
@@ -0,0 +1,73 @@
+import json
+import subprocess
+import os
+
+
+text_api_url = "https://api.openai.com/v1/chat/completions"
+image_api_url = "https://api.openai.com/v1/audio/speech"
+
+api_key = os.environ['OPENAI_API_KEY']
+
+
+def text_text_models(payload):
+  curl_command = [
+      'curl',
+      f'{text_api_url}',
+      '-H', 'Content-Type: application/json',
+      '-H', f'Authorization: Bearer {api_key}',
+      '-d', json.dumps(payload)
+  ]
+  raw = subprocess.run(curl_command, capture_output=True, text=True)
+  response = json.loads(raw.stdout)
+  return response["choices"][0]["message"]["content"]
+
+def invoke_text_model(userPrompt, textModel, temperature):
+  payload = {
+    "model": f"{textModel}",
+    "temperature": temperature,
+    "messages": [{"role": "user", "content": userPrompt}]
+  }  
+  return text_text_models(payload)
+  
+
+def format_audio_dict(deliverable, subject, topic, model, temperature, audioVoice):
+  input = (f"Craft a 125-word monologue on '{topic}', tailored for '{subject}'. "
+   f"Ensure it's insightful, educational, and fitting for a '{deliverable}'. "
+   f"Focus on key concepts and themes in a clear, engaging style, suitable for text-to-speech conversion.")
+  text = invoke_text_model(input, model, temperature)
+  data = {
+      "model": "tts-1-hd",
+      "input": text,
+      "voice": f"{audioVoice}"      
+    }  
+  return data
+
+def text_audio_models(payload):
+  curl_command = [
+      'curl',
+      f'{image_api_url}',
+      '-H', 'Content-Type: application/json',
+      '-H', f'Authorization: Bearer {api_key}',
+      '-d', json.dumps(payload),
+      '-o', './static/output.mp3'
+  ] 
+  raw = subprocess.run(curl_command,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  return raw
+
+
+
+def format_text_dict(deliverable, subject, content, model, temperature, format="HTML"):
+  userPrompt = (f"Develop a {deliverable} guide on '{content}' for high school {subject} teachers. "
+                f"Focus on creating engaging classroom experiences.")
+  outputFormat = f"Format the entire response in {format}."
+  data = {
+          "model": model,
+          "temperature": temperature,
+          "messages": [
+              {"role": "system", "content": "You are the most advanced AI-based NLP engine acting as an expert on the user's prompt topic and as a prompt enhancer"},
+              {"role": "system", "content": "Please DO NOT include links to online resources related to the disscused topic as part of your answer."},          
+              {"role": "system", "content": "Provide an improved prompt at the end of your answer, to be used as preparation for the devised exeperience."},
+              {"role": "user", "content": userPrompt + outputFormat }
+          ]
+  }
+  return data
